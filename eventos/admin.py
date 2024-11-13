06e69d059556ad91b6f8ee0722a0fbc47b23c929
JesusAlmeda03 <55757734+JesusAlmeda03@users.noreply.github.com>
2024-11-13 13:08:44 -0600

    add: viewsets and docs

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Eventos, Participantes, Tutores, Estudiantes
 
 # Register your models here.
+admin.site.register(Eventos)
+admin.site.register(Participantes)
+admin.site.register(Tutores)
+admin.site.register(Estudiantes)
+
+
--- eventos/views.py
+++ eventos/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Eventos, Participantes, Tutores, Estudiantes
+from .serializers import EventosSerializer, ParticipantesSerializer, TutoresSerializer, EstudiantesSerializer
+
+
 
 # Create your views here.
+class EventosViewSet(viewsets.ModelViewSet):
+	queryset = Eventos.objects.all()
+	serializer_class = EventosSerializer
+
+
+
+# Create your views here.
+class ParticipantesViewSet(viewsets.ModelViewSet):
+	queryset = Participantes.objects.all()
+	serializer_class = ParticipantesSerializer
+
+
+
+# Create your views here.
+class TutoresViewSet(viewsets.ModelViewSet):
+	queryset = Tutores.objects.all()
+	serializer_class = TutoresSerializer
+
+
+
+# Create your views here.
+class EstudiantesViewSet(viewsets.ModelViewSet):
+	queryset = Estudiantes.objects.all()
+	serializer_class = EstudiantesSerializer
